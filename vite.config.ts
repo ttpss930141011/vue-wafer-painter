37ca27fb5931b1de9f2160b02d978813104f915a
Justin Xiao <o927416847@gmail.com>
2023-08-26 18:57:17 +0800

    chore: setup cypress and vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from "path";
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -14,5 +15,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "VueWaferPainter",
+      fileName: (format) => `vue-wafer-painter.${format}.js`,
+      formats: ["es", "cjs", "iife"],
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 })
