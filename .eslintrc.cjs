45c6f4c2030165e304af69e13db068b41e6e63e5
Justin Xiao <o927416847@gmail.com>
2023-11-18 16:07:53 -0800

    feat: setup storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,7 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    '@vue/prettier'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting', '@vue/prettier', 'plugin:storybook/recommended'],
   parserOptions: {
     ecmaVersion: 'latest'
   },
