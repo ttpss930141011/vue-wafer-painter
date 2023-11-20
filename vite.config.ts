d4971437a1e4078464a92573c9d16cf05af6ddf9
Justin Xiao <o927416847@gmail.com>
2023-11-20 02:04:54 -0800

    build: support TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      exclude: ['src/stories/**/*', 'src/**/*.stories.ts', 'public']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,10 +23,9 @@ export default defineConfig({
   build: {
     cssCodeSplit: true,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      name: 'VueWaferPainter',
-      fileName: (format) => `vue-wafer-painter.${format}.js`,
-      formats: ['es', 'cjs', 'iife']
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-wafer-painter',
+      fileName: (format) => `vue-wafer-painter.${format}.js`
     },
     rollupOptions: {
       external: ['vue'],
@@ -28,6 +34,8 @@ export default defineConfig({
           vue: 'Vue'
         }
       }
-    }
+    },
+    sourcemap: true,
+    emptyOutDir: true
   }
 })
